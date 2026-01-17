8c14476b3eaaf6c4090b0c2e5afa2f4c1ed544fb
chrismadrigaldev <chrismadrigal.dev@gmail.com>
2026-01-17 07:08:20 -0500

    Changed a few styles and updated the astro.config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     site: "https://www.albcosmetology.com",
     base: "/",
+    devToolbar: {
+        enabled: false,
+    }
 });
