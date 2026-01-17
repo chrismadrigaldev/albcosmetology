4aad1b04f9aba16ac43d6df7fd6b8d3e45678b9e
chrismadrigaldev <chrismadrigal.dev@gmail.com>
2026-01-17 06:38:46 -0500

    Changed information in the astro.config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://albcosmetology.com",
+    site: "https://www.albcosmetology.com",
     base: "/",
 });
