68d976672545ac20cce4da74e72dc95e7be68141
chrismadrigaldev <chrismadrigal.dev@gmail.com>
2026-01-30 18:22:40 -0500

    FIX

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     site: "https://www.albcosmetology.com",
     base: "/",
-    output: "static",
     devToolbar: {
         enabled: false,
     }
